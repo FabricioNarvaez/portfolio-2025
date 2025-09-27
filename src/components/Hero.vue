5fe714425d4f36513a236cb8ffbc38234aecc4df
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-09-27 18:13:45 +0200

    Add animations on hero components

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,15 @@
 <template>
-  <section id="hero" class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-purple-600 text-white text-center">
-    <h1 class="text-5xl md:text-6xl font-bold mb-4">Fabricio Narváez</h1>
-    <p class="text-xl md:text-2xl mb-8">Desarrollador Frontend | Vue.js | API REST</p>
-    <a role="button" @click="scrollToSection('#projects')" class="px-6 py-3 bg-white text-blue-600 rounded-full font-semibold hover:bg-gray-100 transition">Ver Proyectos</a>
-  </section>
+    <section id="hero" class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-purple-600 text-white text-center">
+        <h1 class="text-5xl md:text-6xl font-bold mb-4 
+            opacity-0 translate-y-4 transition-all duration-1000 ease-out animate-hero-fade-in-1">
+            Fabricio Narváez
+        </h1>
+        <p class="text-xl md:text-2xl mb-8 
+            opacity-0 translate-y-4 transition-all duration-1000 ease-out animate-hero-fade-in-2">
+            Desarrollador Frontend | Vue.js | API REST
+        </p>
+        <a role="button" @click="scrollToSection('#projects')" class="animate-bounce px-6 py-3 bg-white text-blue-600 rounded-full font-semibold hover:bg-gray-100 transition">Ver Proyectos</a>
+    </section>
 </template>
 
 <script setup>
